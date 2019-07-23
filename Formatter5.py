ef1c3b45851481594ffa78f557e2abea8a3c2f0a
Yam Sharon <yamsha@post.bgu.ac.il>
2019-07-23 21:05:51 +0300

    Good Overall Graph

---
+++ Formatter5.py
@@ -0,0 +1,82 @@
+import re
+from bs4 import BeautifulSoup as bs
+from Utilities import Utils
+
+
+class Formatter:
+    # Fields:
+    movie_name = None
+    soup = None
+    script_map = None
+
+    # Static:
+    name_format = "^([A-Z]|\\'|\\-|\\&|\\#|[a-z]|\\s|\\.|[0-9])*:"
+    tmp_format = "([A-Z]|\\s|\\&)* "
+
+    def __init__(self, name):
+        self.movie_name = name
+        self.soup = bs(Utils.script(name), features='html.parser')
+        self.script_map = Formatter.get_script_map(self.soup)
+
+    @staticmethod
+    def get_person(line):
+        return re.match(Formatter.name_format, line).group(0)[:-1]
+
+    @staticmethod
+    def get_text(line):
+        return re.split(Formatter.name_format, line).pop().lstrip().lower()
+
+    @staticmethod
+    def get_script_list(lst):
+        ret = []
+        for line in lst:
+            ret.append({'person': Formatter.get_person(line).rstrip(' '), 'text': Formatter.get_text(line)})
+        return ret
+
+    @staticmethod
+    def get_script_map(soup):
+        text = soup.findAll('p')
+        ret = []
+        for td in text:
+            line = td.string
+            if line[0] == '[':
+                continue
+            matcher = re.match(Formatter.tmp_format, line)
+            person = matcher.group(0).rstrip()
+            text = line[line.index(' ') + 1:]
+            ret.append(person + ": " + text)
+        return Formatter.get_script_list(ret)
+
+    def get_char_tokens(self, person):
+        ret = []
+        for col in self.script_map:
+            if col['person'].upper() == person.upper():
+                ret += Utils.get_token(col['text'])
+        return ret
+
+    def get_characters(self):
+        ret = []
+        for col in self.script_map:
+            ret.append(col['person'])
+        return list(dict.fromkeys(ret))  # remove redundancies
+
+    def get_tokens(self):
+        ret = []
+        for col in self.script_map:
+            ret += Utils.get_token(col['text'])  # col['text'].lower().split()
+        return ret
+
+    def get_chars_tuples(self):
+        x = self.get_words_num()
+        x = sorted(x.items(), key=lambda y: y[1])
+        x.reverse()
+        return x  # list(map(lambda tup: tup[0], x))
+
+    def get_words_num(self):
+        my_map = self.script_map
+        ret = {}
+        for col in my_map:
+            x = col['person']
+            count = len(Utils.get_token(col['text']))
+            ret[x] = count if x not in ret.keys() else ret[x] + count
+        return ret
--- Utilities.py
+++ Utilities.py
@@ -1,45 +1,183 @@
-from os import listdir
-from os.path import isfile, join
 
-
-class Data:
+class Utils:
     filesMap = {}
     names = []
+    num_of_formatters = 6
 
     def __init__(self):
-        Data.init_map()
+        Utils.init_map()
 
     @staticmethod
     def set_s(format_index, name):
-        return 'First' if format_index == '1' \
-                   else 'Second', open("Scripts/Format " + format_index + "/" + name + ".html").read()
-
-    @staticmethod
-    def build_map(path):
-        my_dir = "Scripts/" + path
-        files = [f for f in listdir(my_dir) if isfile(join(my_dir, f))]
-        for f in files:
-            file_name = f[0:f.index('.html')]
-            Data.names.append(file_name)
-            Data.filesMap[file_name] = Data.set_s(my_dir[-1], file_name)
+        return format_index, open("Scripts/Format " + format_index + "/" + name + ".html").read()
 
     @staticmethod
     def init_map():
-        Data.build_map("Format 1")
-        Data.build_map("Format 2")
+        for movie in Utils.data['movies']:
+            Utils.names.append(movie['name'])
+            Utils.filesMap[movie['name']] = Utils.set_s(movie['formatter'], movie['name'])
 
     @staticmethod
     def script(name):
-        return Data.filesMap[name][1]
+        return Utils.filesMap[name][1]
 
     @staticmethod
     def formatter(name):
-        return Data.filesMap[name][0]
+        return Utils.filesMap[name][0]
 
     @staticmethod
     def get_token(line):
         def rpl(y): return line.replace(y, ' ')
+
         for x in [',', '.', ';', '?', '!', '/']:
             line = rpl(x)
         return line.split()
 
+    data = {
+        'movies': [
+            {
+                'name': 'Cars 2',
+                'year': 2011,
+                'formatter': '1',
+                'source': 'https://www.imsdb.com/scripts/Cars-2.html'
+            },
+            {
+                'name': 'Frozen',
+                'year': 2013,
+                'formatter': '1',
+                'source': 'https://www.imsdb.com/scripts/Frozen-(Disney).html'
+            },
+            {
+                'name': 'Toy Story',
+                'year': 1995,
+                'formatter': '1',
+                'source': 'https://www.imsdb.com/scripts/Toy-Story.html'
+            },
+            {
+                'name': 'Zootopia',
+                'year': 2016,
+                'formatter': '1',
+                'source': 'https://www.imsdb.com/scripts/Zootopia.html'
+            },
+            {
+                'name': 'Aladdin',
+                'year': 1992,
+                'formatter': '2',
+                'source': 'http://www.fpx.de/fp/Disney/Scripts/Aladdin.txt'
+            },
+            {
+                'name': 'Beauty and the Beast',
+                'year': 1991,
+                'formatter': '2',
+                'source': 'https://www.imsdb.com/scripts/Beauty-and-the-Beast.html'
+            },
+            {
+                'name': 'Hercules',
+                'year': 1997,
+                'formatter': '2',
+                'source': 'http://www.cubbi.org/disney/scripts/hercules.txt'
+            },
+            {
+                'name': 'Jungle Book',
+                'year': 1967,
+                'formatter': '2',
+                'source': 'http://www.cubbi.org/disney/scripts/tjb.txt'
+            },
+            {
+                'name': 'Sleeping Beauty',
+                'year': 1959,
+                'formatter': '2',
+                'source': 'http://www.fpx.de/fp/Disney/Scripts/SleepingBeauty/sb.html'
+            },
+            {
+                'name': '101 Dalmatians',
+                'year': 1961,
+                'formatter': '3',
+                'source': 'https://transcripts.fandom.com/wiki/101_Dalmatians_(1961)'
+            },
+            {
+                'name': 'Brave',
+                'year': 2012,
+                'formatter': '3',
+                'source': 'https://transcripts.fandom.com/wiki/Brave'
+            },
+            {
+                'name': 'Cars',
+                'year': 2006,
+                'formatter': '3',
+                'source': 'https://transcripts.fandom.com/wiki/Cars'
+            },
+            {
+                'name': 'Dumbo',
+                'year': 1941,
+                'formatter': '3',
+                'source': 'https://transcripts.fandom.com/wiki/Dumbo'
+            },
+            {
+                'name': 'Inside Out',
+                'year': 2015,
+                'formatter': '3',
+                'source': 'https://transcripts.fandom.com/wiki/Inside_Out'
+            },
+            {
+                'name': 'Moana',
+                'year': 2016,
+                'formatter': '3',
+                'source': 'https://transcripts.fandom.com/wiki/Moana'
+            },
+            {
+                'name': 'The Aristocrats',
+                'year': 1970,
+                'formatter': '3',
+                'source': 'https://transcripts.fandom.com/wiki/The_Aristocats'
+            },
+            {
+                'name': 'Toy Story 2',
+                'year': 1999,
+                'formatter': '3',
+                'source': 'https://transcripts.fandom.com/wiki/Toy_Story_2'
+            },
+            {
+                'name': 'Toy Story 3',
+                'year': 2010,
+                'formatter': '3',
+                'source': 'https://transcripts.fandom.com/wiki/Toy_Story_3'
+            },
+            {
+                'name': 'Mulan',
+                'year': 1998,
+                'formatter': '4',
+                'source': 'http://www.fpx.de/fp/Disney/Scripts/Mulan.html'
+            },
+            {
+                'name': 'The Little Mermaid',
+                'year': 1989,
+                'formatter': '4',
+                'source': 'https://www.imsdb.com/scripts/Little-Mermaid,-The.html'
+            },
+            {
+                'name': 'Pocahontas',
+                'year': 1995,
+                'formatter': '5',
+                'source': 'https://transcripts.fandom.com/wiki/Pocahontas'
+            },
+            {
+                'name': 'Alice in Wonderland',
+                'year': 1951,
+                'formatter': '6',
+                'source': 'https://transcripts.fandom.com/wiki/Alice_in_Wonderland_(1951)'
+            },
+            {
+                'name': 'Finding Nemo',
+                'year': 2003,
+                'formatter': '6',
+                'source': 'https://transcripts.fandom.com/wiki/Finding_Nemo'
+            },
+            {
+                'name': 'Toy Story 4',
+                'year': 2019,
+                'formatter': '6',
+                'source': 'https://disneyfanon.fandom.com/wiki/Toy_Story_4_transcript'
+            },
+        ]
+    }
